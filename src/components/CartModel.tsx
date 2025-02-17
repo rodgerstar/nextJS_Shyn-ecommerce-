ba605e096b43c93a8b9e37d66d678d9c1fc7f8d5
rodgers <rodgerstarrodgers@gmail.com>
2025-02-17 16:51:59 +0300

    Navbar

--- src/components/CartModel.tsx
+++ src/components/CartModel.tsx
@@ -1,11 +1,95 @@
 "use client"
 
+import Image from "next/image";
+
 const CartModel = () => {
-  return (
-      <div className=''>
-        Cart model
-      </div>
-  )
+
+    const cartItems = true
+
+    return (
+        <div
+            className='w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20'>
+            {!cartItems ? (
+                <div className=''>Cart is Empty</div>
+            ) : (
+                <>
+                    <h2 className='text-xl'>Shopping Cart</h2>
+                <div className='flex flex-col gap-8'>
+                    {/*Item*/}
+                    <div className=' flex gap-4'>
+                        <Image src="/girl.jpg"
+                               alt=""
+                               width={72}
+                               height={96}
+                               className='object-cover rounded-md'
+                        />
+                        <div className='flex flex-col justify-between w-full'>
+                            {/*Top*/}
+                            <div>
+                                {/*title*/}
+                                <div className='flex items-center justify-between gap-8 '>
+                                    <h3 className='font-semibold'>Product Name</h3>
+                                    <div className='p-1 bg-gray-50 rounded-sm'>Ksh. 500</div>
+                                </div>
+                                {/*desc*/}
+                                <div className='text-sm text-gray-500'>
+                                    available
+                                </div>
+                            </div>
+                            {/*Bottom*/}
+                            <div className=' flex justify-between text-sm'>
+                                <span className='text-gray-500'>Qty. 2</span>
+                                <span className='text-blue-500'>Remove</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className=' flex gap-4'>
+                        <Image src="/girl.jpg"
+                               alt=""
+                               width={72}
+                               height={96}
+                               className='object-cover rounded-md'
+                        />
+                        <div className='flex flex-col justify-between w-full'>
+                            {/*Top*/}
+                            <div>
+                                {/*title*/}
+                                <div className='flex items-center justify-between gap-8 '>
+                                    <h3 className='font-semibold'>Product Name</h3>
+                                    <div className='p-1 bg-gray-50 rounded-sm'>Ksh. 500</div>
+                                </div>
+                                {/*desc*/}
+                                <div className='text-sm text-gray-500'>
+                                    available
+                                </div>
+                            </div>
+                            {/*Bottom*/}
+                            <div className=' flex justify-between text-sm'>
+                                <span className='text-gray-500'>Qty. 2</span>
+                                <span className='text-blue-500'>Remove</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+               {/*bottom*/}
+               <div className=''>
+                   <div className=' flex items-center justify-between font-semibold'>
+                       <span>Subtotal</span>
+                       <span>Ksh. 2000</span>
+                   </div>
+                   <p className='text-gray-500 text-sm mt-2 mb-4 '>
+                       Shipping and taxes calculated at Checkout.
+                   </p>
+                   <div className='flex justify-between text-sm'>
+                       <button className='rounded-md py-3 px-4 ring-1 ring-gray-300'>View Cart</button>
+                       <button className='rounded-md py-3 px-4 bg-black text-white'>View Cart</button>
+                   </div>
+               </div>
+
+             </>
+            )}
+        </div>
+    )
 }
 
 export default CartModel
