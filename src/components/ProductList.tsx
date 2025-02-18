d2bcf259d00821c96a992dd37628b3e48c47ee59
rodgers <rodgerstarrodgers@gmail.com>
2025-02-18 23:29:34 +0300

    productlist

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,115 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const ProductList = () => {
+    return (
+        <div className='mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap'>
+            <Link href="/test" className='w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]'>
+                <div className='relative w-full h-80'>
+                    <Image
+                        src='/girl.jpg'
+                        alt="Girl with scarf"
+                        fill
+                        sizes="25vw"
+                        className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+                    />
+                    <Image
+                        src='/scarf.png'
+                        alt="Scarf"
+                        fill
+                        sizes="25vw"
+                        className='absolute object-cover rounded-md '
+                    />
+                </div>
+                <div className='flex justify-between'>
+                    <span className='font-medium'>Product Name</span>
+                    <span className='font-semibold'>KES. 500</span>
+                </div>
+                <div className='text-sm text-gray-500'>
+                    My Description
+                </div>
+                <button className='rounded-2xl ring-1 w-max ring-reed text-reed py-2 px-4 text-xs hover:bg-reed hover:text-white'>Add To Cart</button>
+            </Link>
+            <Link href="/test" className='w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]'>
+                <div className='relative w-full h-80'>
+                    <Image
+                        src='/girl.jpg'
+                        alt="Girl with scarf"
+                        fill
+                        sizes="25vw"
+                        className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+                    />
+                    <Image
+                        src='/scarf.png'
+                        alt="Scarf"
+                        fill
+                        sizes="25vw"
+                        className='absolute object-cover rounded-md '
+                    />
+                </div>
+                <div className='flex justify-between'>
+                    <span className='font-medium'>Product Name</span>
+                    <span className='font-semibold'>KES. 500</span>
+                </div>
+                <div className='text-sm text-gray-500'>
+                    My Description
+                </div>
+                <button className='rounded-2xl ring-1 w-max ring-reed text-reed py-2 px-4 text-xs hover:bg-reed hover:text-white'>Add To Cart</button>
+            </Link>
+            <Link href="/test" className='w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]'>
+                <div className='relative w-full h-80'>
+                    <Image
+                        src='/girl.jpg'
+                        alt="Girl with scarf"
+                        fill
+                        sizes="25vw"
+                        className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+                    />
+                    <Image
+                        src='/scarf.png'
+                        alt="Scarf"
+                        fill
+                        sizes="25vw"
+                        className='absolute object-cover rounded-md '
+                    />
+                </div>
+                <div className='flex justify-between'>
+                    <span className='font-medium'>Product Name</span>
+                    <span className='font-semibold'>KES. 500</span>
+                </div>
+                <div className='text-sm text-gray-500'>
+                    My Description
+                </div>
+                <button className='rounded-2xl ring-1 w-max ring-reed text-reed py-2 px-4 text-xs hover:bg-reed hover:text-white'>Add To Cart</button>
+            </Link>
+            <Link href="/test" className='w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]'>
+                <div className='relative w-full h-80'>
+                    <Image
+                        src='/girl.jpg'
+                        alt="Girl with scarf"
+                        fill
+                        sizes="25vw"
+                        className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+                    />
+                    <Image
+                        src='/scarf.png'
+                        alt="Scarf"
+                        fill
+                        sizes="25vw"
+                        className='absolute object-cover rounded-md '
+                    />
+                </div>
+                <div className='flex justify-between'>
+                    <span className='font-medium'>Product Name</span>
+                    <span className='font-semibold'>KES. 500</span>
+                </div>
+                <div className='text-sm text-gray-500'>
+                    My Description
+                </div>
+                <button className='rounded-2xl ring-1 w-max ring-reed text-reed py-2 px-4 text-xs hover:bg-reed hover:text-white'>Add To Cart</button>
+            </Link>
+        </div>
+    )
+}
+
+export default ProductList;
