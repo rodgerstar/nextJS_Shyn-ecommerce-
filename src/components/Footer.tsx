354dbda77df73105f08b15c26ffeb885deb44138
rodgers <rodgerstarrodgers@gmail.com>
2025-02-19 19:01:49 +0300

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,97 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const Footer = () => {
   return (
-      <div className=''>
-        Footer
+    <div className='py-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 bg-gray-100 text-sm mt-24'>
+      {/* Top */}
+      <div className='flex flex-col md:flex-row justify-between gap-24'>
+        {/* Left */}
+        <div className='w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8'>
+          <Link href="/">
+            <div className='text-2xl tracking-wide'>SHYN</div>
+          </Link>
+          <p>325 Kimathi Street, Central Plaza, Nairobi. CA 90210 Kenya</p>
+          <span className='font-semibold'>Info@Shyn.com</span>
+          <span className='font-semibold'>+254-758-023-590</span>
+          <div className='flex gap-6'>
+            <Image src="/facebook.png" alt="Facebook" width={16} height={16}/>
+            <Image src="/instagram.png" alt="Instagram" width={16} height={16}/>
+            <Image src="/youtube.png" alt="YouTube" width={16} height={16}/>
+            <Image src="/pinterest.png" alt="Pinterest" width={16} height={16}/>
+            <Image src="/x.png" alt="X" width={16} height={16}/>
+          </div>
+        </div>
+
+        {/* Center - Currently hidden on smaller screens */}
+        <div className='w-1/2 hidden lg:flex justify-between'>
+          <div className='flex flex-col justify-between'>
+            <h1 className='font-medium text-lg'>COMPANY</h1>
+            <div className='flex flex-col gap-6'>
+              <Link href="">About Us</Link>
+              <Link href="">Careers</Link>
+              <Link href="">Affiliates</Link>
+              <Link href="">Blog</Link>
+              <Link href="">Contact Us</Link>
+            </div>
+          </div>
+          <div className='flex flex-col justify-between'>
+            <h1 className='font-medium text-lg'>SHOP</h1>
+            <div className='flex flex-col gap-6'>
+              <Link href="">New Arrivals</Link>
+              <Link href="">Accessories</Link>
+              <Link href="">Men</Link>
+              <Link href="">Women</Link>
+              <Link href="">All Products</Link>
+            </div>
+          </div>
+          <div className='flex flex-col justify-between'>
+            <h1 className='font-medium text-lg'>HELP</h1>
+            <div className='flex flex-col gap-6'>
+              <Link href="">Customer Service</Link>
+              <Link href="">My Account</Link>
+              <Link href="">Find a Store</Link>
+              <Link href="">Privacy</Link>
+              <Link href="">Gift Card</Link>
+            </div>
+          </div>
+        </div>
+
+        {/* Right */}
+        <div className='w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8'>
+          <h1 className='font-medium text-lg'>SUBSCRIBE</h1>
+          <p>Be the first to get the latest news about trends, promotions, and much more!</p>
+          <div className='flex'>
+            <input type="text" placeholder='Email Address' className='p-4 w-3/4'/>
+            <button className='w-1/4 bg-reed text-white'>JOIN</button>
+          </div>
+          <span className='font-semibold'>Secure Payments</span>
+          <div className='flex justify-between'>
+            <Image src="/discover.png" alt="Discover" width={40} height={20}/>
+            <Image src="/skrill.png" alt="Skrill" width={40} height={20}/>
+            <Image src="/paypal.png" alt="PayPal" width={40} height={20}/>
+            <Image src="/mastercard.png" alt="Mastercard" width={40} height={20}/>
+            <Image src="/visa.png" alt="Visa" width={40} height={20}/>
+          </div>
+        </div>
       </div>
+
+      {/* Bottom */}
+      <div className=' flex flex-col md:flex-row items-center justify-between gap-8 mt-16'>
+        <div className=''>© 2024 SHYN Closet | powered by NEXEL</div>
+        <div className=' flex flex-col gap-8 md:flex-row'>
+          <div className=''>
+            <span className='text-gray-500 mr-4'>Language</span>
+            <span className='font-medium'>Kenya | English</span>
+          </div>
+          <div className=''>
+            <span className='text-gray-500 mr-4'>Currency</span>
+            <span className='font-medium'>KES</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Footer
+export default Footer;
