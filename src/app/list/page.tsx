e07cce0272c7b0b3cf79536d2e3cac932fc98483
rodgers <rodgerstarrodgers@gmail.com>
2025-02-20 19:51:40 +0300

    list page

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
+import Filter from "@/components/Filter";
+import ProductList from "@/components/ProductList";
+
 const ListPage = () => {
   return (
-      <div className=''>
-        ListPage
+      <div className='px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative'>
+          {/*campaign*/}
+          <div className='hidden bg-pink-50 p-4 px-4 sm:flex justify-between h-64'>
+              <div className='w-2/3 flex flex-col items-center justify-center gap-8'>
+                  <h1 className='text-4xl font-semibold leading-[48px] text-gray-700 '>Grab up to 50% off on <br/>
+                      selected products</h1>
+                  <button className='rounded-3xl bg-reed text-white w-max py-3 px-5 text-sm'>Buy Now</button>
+              </div>
+              <div className='relative w-1/3'>
+                  <Image src="/woman.png" alt="" fill className='object-contain'/>
+              </div>
+          </div>
+          {/*filter*/}
+          <Filter/>
+          {/*products*/}
+          <h1 className='mt-12 text-xl font-semibold'>Shoes for You!</h1>
+          <ProductList/>
+          <h1></h1>
       </div>
   )
 }
---
+++ src/components/Filter.tsx
@@ -0,0 +1,31 @@
+const Filter = () => {
+  return (
+      <div className='mt-12 flex justify-between'>
+          <div className='flex gap-6'>
+              <select name="type" id="" className='py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]'>
+                  <option></option>
+                  <option value="physical">Physical</option>
+                  <option value="digital">Digital</option>
+              </select>
+              <select name="type" id="" className='py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]'>
+                  <option></option>
+                  <option value="physical">Physical</option>
+                  <option value="digital">Digital</option>
+              </select>
+              <select name="type" id="" className='py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]'>
+                  <option></option>
+                  <option value="physical">Physical</option>
+                  <option value="digital">Digital</option>
+              </select>
+              <select name="type" id="" className='py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]'>
+                  <option></option>
+                  <option value="physical">Physical</option>
+                  <option value="digital">Digital</option>
+              </select>
+          </div>
+          <div className=''></div>
+      </div>
+  )
+}
+
+export default Filter
